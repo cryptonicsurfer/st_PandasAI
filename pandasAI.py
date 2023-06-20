595e3cbf24f2955062ad3258e1641c28e960ad7f
Frej Andreassen <frej.andreassen@gmail.com>
2023-06-20 15:31:15 +0200

    Updates

--- pandasAI.py
+++ pandasAI.py
@@ -31,7 +31,7 @@ variables = {}
 # Parse each line in the text file
 for line in lines:
     # Split the line into a name and a value
-    name, value = line.split('=')
+    name, value = line.split('=', 1)
     
     # Remove unnecessary characters like quotation marks and newline characters
     name = name.strip()
@@ -39,7 +39,6 @@ for line in lines:
     
     # Add the name and value to the dictionary
     variables[name] = value
-
 
 col1, col2 = st.columns(2)
 
@@ -57,14 +56,16 @@ with col1:
     data = response.json()
 
     #extract the "data" field from the response to an array
-    data=data['data']
+    data = data['data']
 
     # Convert the list of dictionaries to a Pandas DataFrame
     df = pd.DataFrame(data)
+    # Make all column names lowercase
+    df.columns = df.columns.str.lower()
 
     # Create a multi-select box for selecting columns and convert dict to df
-    selected_columns = st.multiselect("Select columns to keep", df.columns.tolist())
-    df = df[selected_columns]
+    # selected_columns = st.multiselect("Select columns to keep", df.columns.tolist())
+    # df = df[selected_columns]
 
     if 'datum' in df.columns:
         df['datum'] = pd.to_datetime(df['datum'])
@@ -73,29 +74,28 @@ with col1:
     # Display the DataFrame in Streamlit
     st.write(df.head(4))
 
-# Create a multi-select box for selecting the category column
-category_column = st.selectbox("Select the category column", df.columns.tolist())
+    # Create a multi-select box for selecting the category column
+    category_columns = st.multiselect("Select the category column(s)", df.columns.tolist())
 
-# Create a multi-select box for selecting the value column
-value_column = st.selectbox("Select the value column", df.columns.tolist())
+    # Create a multi-select box for selecting the value column
+    value_columns = st.multiselect("Select the value column(s)", df.columns.tolist())
 
-# Pivot your DataFrame if the selected category and value columns are in the DataFrame
-if {category_column, value_column}.issubset(df.columns):
-    df_pivot = df.pivot(index='datum', columns=category_column, values=value_column)
+    # Create a dictionary for the aggregation methods
+    agg_methods = {"sum": np.sum, "mean": np.mean, "max": np.max, "min": np.min}
+
+    # Dropdown to select aggregation method
+    agg_method = st.selectbox("Select aggregation method", list(agg_methods.keys()))
+
+    # if category_columns and value_columns:
+    # Pivot your DataFrame if the selected category and value columns are in the DataFrame
+    df_pivot = df.pivot_table(index='datum', values=value_columns, columns=category_columns,  aggfunc=agg_methods[agg_method])
 
     # Display the pivoted DataFrame in Streamlit
     st.write(df_pivot.head(4))
 
-    # Create a multi-select box for selecting categories to display
-    selected_categories = st.multiselect("Select categories to display", df[category_column].unique().tolist())
-
-    # Filter the pivoted DataFrame to only include the selected categories
-    df_pivot = df_pivot[selected_categories]
 
     # Draw a line chart for your pivoted DataFrame
     st.line_chart(df_pivot)
-    
-
 
 with col2:
     prompt = st.text_area("skriv din fråga")
@@ -106,5 +106,3 @@ with col2:
             st.write(pandas_ai.run(df, prompt=prompt))
         else:
             st.warning("skriv in din fråga")
-
-
